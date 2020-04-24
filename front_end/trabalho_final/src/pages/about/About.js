7143db34ec0ca761d36622b9ad71be3828154fe4
Miguel Tomé <aluno19993@ipt.pt>
2020-04-24 11:07:39 +0100

    inicio  do projeto em react
    Inicio do projeto em React

---
+++ front_end/trabalho_final/src/pages/about/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AboutPage extends React.Component {
+  render () {
+    return (
+      <div className="App" id="about-board">
+        <h3>Hi! We're in About</h3>
+      </div>
+    );
+  }
+}
