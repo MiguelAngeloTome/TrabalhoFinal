e8bcd34972f1b4d0e2920cacb8876047fd901a2e
Andre Luis Fronteira <aluno20160@ipt.pt>
2020-05-05 02:37:18 +0100

    Alteracao da base de dados
    Adicao de tabelas em falta

--- back_end/src/configs/mysql.js
+++ back_end/src/configs/mysql.js
@@ -10,8 +10,32 @@ const db = new sqlite3.Database(
     }
 );
 
+db.run(`create table if not exists user(
+        'user_id' varchar(36) primary key,
+        'username' varchar(30),
+        'password' varchar(200),
+        'email' varchar(30),
+        'dataIv' varchar(200),
+        'name' varchar(30),
+        'type' varchar(30))`
+);
+
+db.run(`create table if not exists vinha(
+        'vinha_id' varchar(36) primary key,
+        'localizacao' varchar(30),
+        'dono' varchar(30))`
+);
+
+db.run(`create table if not exists module(
+        'module_id' varchar(36) primary key,
+        'vinha_id' varchar(36),
+        'localizacao' varchar(30),
+        foreign key (vinha_id) REFERENCES vinha(vinha_id))`
+);
+
 db.run(`create table if not exists data(
         'data_id' varchar(36) primary key,
+        'module_id' varchar(36),
         'date' varchar(30),
         'temp' float,
         'air_humidity' float,
@@ -20,30 +44,16 @@ db.run(`create table if not exists data(
         'pluviosidade' float,
         'vel_vento' float,
         'dir_vento' varchar(10),
-        'radiacao' float)`
-);
-
-db.run(`create table if not exists user(
-        'user_id' varchar(36) primary key,
-        'username' varchar(30),
-        'password' varchar(200),
-        'email' varchar(30),
-        'dataIv' varchar(200),
-        'name' varchar(30))`
-);
-
-db.run(`create table if not exists module(
-        'module_id' varchar(36) primary key,
-        'localizacao' varchar(30),
-        'dono' varchar(30))`
-);
-
-db.run(`create table if not exists user_module(
-        'user_id' varchar(36),
-        'module_id' varchar(36),
-        primary key (user_id, module_id),
-        foreign key (user_id) REFERENCES user(user_id),
+        'radiacao' float,
         foreign key (module_id) REFERENCES module(module_id))`
+);
+
+db.run(`create table if not exists vinha_user(
+        'user_id' varchar(36),
+        'vinha_id' varchar(36),
+        primary key (user_id, vinha_id),
+        foreign key (user_id) REFERENCES user(user_id),
+        foreign key (vinha_id) REFERENCES vinha(vinha_id))`
 );
 
 module.exports = db;
